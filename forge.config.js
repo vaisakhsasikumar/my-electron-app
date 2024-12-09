d201370a6c86b3fbf923442c7da190e3cde2e1cf
andrei-kim-3tsoftwarelabs <andrei.kim@3tsoftwarelabs.com>
2024-12-09 14:43:23 +0100

    fix builds

--- forge.config.js
+++ forge.config.js
@@ -4,7 +4,6 @@ const { FuseV1Options, FuseVersion } = require('@electron/fuses');
 module.exports = {
   packagerConfig: {
     asar: true,
-    extraResource: ["/backend/"]
   },
   rebuildConfig: {},
   makers: [
